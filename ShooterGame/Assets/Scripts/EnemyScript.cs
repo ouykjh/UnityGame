3985f9924c2adb6d9ab2821fd64676c68b14814a
Kuba Wilczek <jak_wil@wp.pl>
2016-06-06 21:00:45 +0200

    game  prototype

---
+++ ShooterGame/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    private bool hasSpawn;
+    private MoveScript moveScript;
+    private WeaponScript[] weapons;
+
+    void Awake()
+    {
+        weapons = GetComponentsInChildren<WeaponScript>();
+        moveScript = GetComponent<MoveScript>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        hasSpawn = false;
+        GetComponent<Collider2D>().enabled = false;
+        moveScript.enabled = false;
+        foreach (WeaponScript weapon in weapons)
+        {
+            weapon.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasSpawn == false)
+        {
+            if (GetComponent<Renderer>().IsVisibleFrom(Camera.main))
+            {
+                Spawn();
+            }
+        }
+        else
+        {
+            foreach (WeaponScript weapon in weapons)
+            {
+
+                if (weapon != null && weapon.CanAttack && weapon.enabled)
+                {
+                    weapon.Attack(true);
+                }
+            }
+
+            if (GetComponent<Renderer>().IsVisibleFrom(Camera.main) == false)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    private void Spawn()
+    {
+        hasSpawn = true;
+
+        GetComponent<Collider2D>().enabled = true;
+        moveScript.enabled = true;
+        foreach (WeaponScript weapon in weapons)
+        {
+          weapon.enabled = true;
+        }
+    }
+}
---
+++ ShooterGame/Assets/Scripts/SpecialEffectsHelper.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Creating instance of particles from code with no effort
+/// </summary>
+public class SpecialEffectsHelper : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public static SpecialEffectsHelper Instance;
+
+    public ParticleSystem smokeEffect;
+    public ParticleSystem fireEffect;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of SpecialEffectsHelper!");
+        }
+
+        Instance = this;
+    }
+    public void Explosion(Vector3 position)
+    {
+        instantiate(smokeEffect, position);
+        instantiate(fireEffect, position);
+    }
+
+ 
+    private ParticleSystem instantiate(ParticleSystem prefab, Vector3 position)
+    {
+        ParticleSystem newParticleSystem = Instantiate(
+          prefab,
+          position,
+          Quaternion.identity
+        ) as ParticleSystem;
+
+        Destroy(
+          newParticleSystem.gameObject,
+          newParticleSystem.startLifetime
+        );
+
+        return newParticleSystem;
+    }
+}
